e183820efb31a043bd7549a42eb792d8af712ee7
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-11-28 23:43:37 +0300

    Create LookupHost_usage.go

---
+++ LookupHost_usage.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage : %s ipAddr\n", os.Args[0])
+		os.Exit(0)
+	}
+	address := os.Args[1]
+
+	host, err := net.LookupHost(address)
+
+	if err != nil {
+		log.Fatal("Failed ...")
+		os.Exit(0)
+	}
+
+	for _, v := range host {
+		fmt.Fprintf(os.Stderr, "Host : %s\n", v)
+	}
+
+	os.Exit(0)
+}
+Host : 216.58.214.132 // ipv4
+Host : 2a00:1450:4017:800::2004 // ipv6 address
+
+/*
+RESULT 
+
+
+
+*/
